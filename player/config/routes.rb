4b019cf5211aa2830dcb6fb8450d621298c9ffe9
dakotameadows <dakota.w.meadows@gmail.com>
2014-12-18 17:49:32 -0500

    working on query display

--- player/config/routes.rb
+++ player/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  match "/welcome/play" => "welcome#play", :as => 'play', via: [:get, :post]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
