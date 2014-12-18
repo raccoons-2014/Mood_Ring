37eac5ed53eef259c97c7bf670d9148edb2cff68
dakotameadows <dakota.w.meadows@gmail.com>
2014-12-18 17:49:09 -0500

    Randomizer almost complete

--- player/app/assets/javascripts/player.js
+++ player/app/assets/javascripts/player.js
@@ -1,6 +1,16 @@
   $(document).ready(function() {
 
-  SC.stream("/tracks/293", function(sound){
+    SC.initialize({
+      client_id: '5b91135eafaf701ea414c5fe6b86fdf3'
+    });
+    // find all sounds of buskers licensed under 'creative commons share alike'
+    songID = SC.get('/tracks', { bpm:{from: 120} }, function(tracks) {
+      console.log(tracks[Math.floor(Math.random()*tracks.length)].id);
+      return tracks[Math.floor(Math.random()*tracks.length)].id;
+    });
+
+
+  SC.stream("/tracks/" + songID, function(sound){
     sound.play();
   });
 
