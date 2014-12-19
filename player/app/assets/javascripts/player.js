21dbd9dfffb32f6fd50572dfd6b7eeb1aa71079c
Karina Carmona <karinacarmona@me.com>
2014-12-19 08:54:10 -0500

    pause button and play button working

--- player/app/assets/javascripts/player.js
+++ player/app/assets/javascripts/player.js
@@ -1,16 +1,40 @@
 $(document).ready(function(){
 
-  SC.get('/tracks', { q: 'buskers', license: 'cc-by-sa' }, function(tracks) {
-    var ID = tracks[Math.floor(Math.random()*tracks.length)].id;
-    current_track = 'https://api.soundcloud.com/tracks/' + ID + '/stream'
+  getTracks();
 
-  });
+  // var someSound = soundManager.createSound({
+  //   id: "sound",
+  //   url: current_track
+  // });
 
 
-  $('#play').click(function(event) {
-  SC.stream(current_track, function(sound){
-    sound.play();
-  });
-  })
+
+  // $('#pause').click(function(event){
+  //   SC.stream(current_track, function(sound){
+  //     sound.pause();
+  //   });
+  // })
 
 })
+
+var getTracks= function() {
+ SC.get('/tracks', { q: 'buskers', license: 'cc-by-sa' }, function(tracks) {
+    var ID = tracks[Math.floor(Math.random()*tracks.length)].id;
+    current_track = 'https://api.soundcloud.com/tracks/' + ID + '/stream'
+    console.log(current_track);
+    streamSongs();
+  });
+}
+
+var streamSongs =  function(){
+  console.log('I"m getting here first!')
+  SC.stream(current_track, function(sound){
+    $('#play').click(function(event) {
+      sound.play();
+    }),
+    $('#pause').click(function(event){
+      sound.pause();
+      console.log("pause");
+    });
+  });
+}
