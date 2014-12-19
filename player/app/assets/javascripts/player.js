578e696dc83cd5c92d6da2908542ab7b4a78718b
Karina Carmona <karinacarmona@me.com>
2014-12-19 11:46:22 -0500

    Added structure for new oOJS player

--- player/app/assets/javascripts/player.js
+++ player/app/assets/javascripts/player.js
@@ -10,13 +10,34 @@ var getTracks= function() {
   });
 }
 
-var streamSongs =  function(){
+function PlayerWidget = {
+  this.current_track="";
+  this.tracks_array= [];
+}
+PlayerWidget.prototype.getParameters= function {
+
+}
+
+PlayerWidget.prototype.fetchSongArray= function{
+//get song array
+}
+
+PlayerWidget.prototype.setCurrentTrack = function {
+//move throughout array
+}
+
+PlayerWidget.prototype.streamSong = function{
+  //general play functions ... add next button
   SC.stream(current_track, function(sound){
-    $('#play').click(function(event) {
-      sound.play();
-    }),
-    $('#pause').click(function(event){
-      sound.pause();
-    });
+  $('#play').click(function(event) {
+    sound.play();
+  }),
+  $('#pause').click(function(event){
+    sound.pause();
   });
+});
+}
+
+PlayerWidget.prototype.nextSongFetch = function {
+//get next song once first one finishes
 }
