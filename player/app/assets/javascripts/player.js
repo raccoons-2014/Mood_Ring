2e068053041ac1916fcc8e0f6bdae5beb654736f
dakotameadows <dakota.w.meadows@gmail.com>
2014-12-18 22:40:09 -0500

    listen and play buttons working

--- player/app/assets/javascripts/player.js
+++ player/app/assets/javascripts/player.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
-   SC.get('/tracks', { q: 'buskers', license: 'cc-by-sa' }, function(tracks) {
-      var ID = tracks[Math.floor(Math.random()*tracks.length)].id;
-      current_track = 'https://api.soundcloud.com/tracks/' + ID + '/stream'
-    });
+
+  SC.get('/tracks', { tags: "rap" }, function(tracks) {
+    var ID = tracks[Math.floor(Math.random()*tracks.length)].id;
+    current_track = 'https://api.soundcloud.com/tracks/' + ID + '/stream'
+
+  });
+
 
   $('#play').click(function(event) {
   SC.stream(current_track, function(sound){
     sound.play();
   });
   })
-}
 
-
+})
