b621675e56a259905ca0ac433e45a30dfe24e25a
Karina Carmona <karinacarmona@me.com>
2014-12-19 12:59:08 -0500

    fetch tags from Ui and populate Track URLS

--- player/app/assets/javascripts/player.js
+++ player/app/assets/javascripts/player.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  getTracks();
+
+ //on submit get paramter, then fetchsongArrary, them stream songs, then play next, repeat
 })
 
 var getTracks= function() {
@@ -10,23 +11,32 @@ var getTracks= function() {
   });
 }
 
-function PlayerWidget = {
-  this.current_track="";
-  this.tracks_array= [];
-}
-PlayerWidget.prototype.getParameters= function {
-
+function PlayerWidget(sourceSeletor) {
+  this.sourceSelctor = sourceSelctor;
+  this.trackUrls = [];
+  this.populateTrackUrls(this.getTagName());
 }
 
-PlayerWidget.prototype.fetchSongArray= function{
+// find params through input field
+PlayerWidget.prototype.getTagName= function() {
+  return document.getElementById(this.sourceSelctor).value
+}
+
 //get song array
+PlayerWidget.prototype.populateTrackUrls= function(params) {
+  SC.get('/tracks', { tags: params }, function(tracks) {
+    for (i=0; i <tracks.length; i++) {
+      this.trackUrls.push(tracks[i].stream_url);
+    };
+    this.current_track = this.trackUrls[0];
+  }.bind(this));
 }
 
-PlayerWidget.prototype.setCurrentTrack = function {
+PlayerWidget.prototype.setCurrentTrack = function() {
 //move throughout array
 }
 
-PlayerWidget.prototype.streamSong = function{
+PlayerWidget.prototype.streamSong = function() {
   //general play functions ... add next button
   SC.stream(current_track, function(sound){
   $('#play').click(function(event) {
@@ -38,6 +48,6 @@ PlayerWidget.prototype.streamSong = function{
 });
 }
 
-PlayerWidget.prototype.nextSongFetch = function {
+PlayerWidget.prototype.nextSongFetch = function() {
 //get next song once first one finishes
 }
