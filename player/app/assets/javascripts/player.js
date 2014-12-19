86f556fd37d4c9b227b270382b15e372cdf31591
Karina Carmona <karinacarmona@me.com>
2014-12-18 21:10:39 -0500

    basic play function with randomizing song working

--- player/app/assets/javascripts/player.js
+++ player/app/assets/javascripts/player.js
@@ -1,48 +1,49 @@
 $(document).ready(function(){
 
-  n = new MusicPlayer()
-  // n.play('#play');
-  // n.stop('#pause');
+  SC.get('/tracks', { q: 'buskers', license: 'cc-by-sa' }, function(tracks) {
+    var ID = tracks[Math.floor(Math.random()*tracks.length)].id;
+    current_track = 'https://api.soundcloud.com/tracks/' + ID + '/stream'
 
-  SC.initialize ({
-    client_id: '5b91135eafaf701ea414c5fe6b86fdf3'
   });
 
-  // SC.get('/tracks', { q: 'buskers', license: 'cc-by-sa' }, function(tracks) {
-  //   var ID = tracks[Math.floor(Math.random()*tracks.length)].id;
-  //   n.updateSongID(ID);
-  //   n.updateUrl();
-  // });
-  // n.play('#play');
-  // n.stop('#pause')
-})
 
-$('#play').click(function(event) {
-  n.play();
-})
-
-MusicPlayer = function() {
-  this.songID = null;
-  this.url = '';
-}
-
-//To refactor
-MusicPlayer.prototype.play = function(){
-  SC.stream('https://api.soundcloud.com/tracks/182163764/stream', function(sound){
+  $('#play').click(function(event) {
+  SC.stream(current_track, function(sound){
     sound.play();
   });
-}
-
-MusicPlayer.prototype.stop = function(button){
-  $(button).on('click', function(){
-    soundManager.pauseAll();
   })
-}
 
-MusicPlayer.prototype.updateSongID = function(ID) {
-  this.songID = ID;
-}
+})
 
-MusicPlayer.prototype.updateUrl = function() {
-  this.url = 'https://api.soundcloud.com/tracks/' + this.songID + '/stream';
-}
+
+  // n.play('#play');
+  // n.stop('#pause')
+// $('#play').click(function(event) {
+//   n.play();
+// })
+
+// MusicPlayer = function() {
+//   this.songID = null;
+//   this.url = '';
+// }
+
+// //To refactor
+// MusicPlayer.prototype.play = function(){
+//   SC.stream('https://api.soundcloud.com/tracks/182163764/stream', function(sound){
+//     sound.play();
+//   });
+// }
+
+// MusicPlayer.prototype.stop = function(button){
+//   $(button).on('click', function(){
+//     soundManager.pauseAll();
+//   })
+// }
+
+// MusicPlayer.prototype.updateSongID = function(ID) {
+//   this.songID = ID;
+// }
+
+// MusicPlayer.prototype.updateUrl = function() {
+//   this.url = 'https://api.soundcloud.com/tracks/' + this.songID + '/stream';
+// }
