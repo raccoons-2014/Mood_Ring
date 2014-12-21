64ea50a4eaa27a75f7757aefb58cf8b3751b5d87
maggie <myustinreyes@gmail.com>
2014-12-21 08:56:39 -0500

    dummy test file working

--- player/spec/javascripts/player_spec.js
+++ player/spec/javascripts/player_spec.js
@@ -9,5 +9,8 @@ describe('player', function() {
   it("selects the id of sad", function() {
   	console.log(this.player)
     expect(player.sourceSelector).toBe('sad')
+	var orange = 'orange';
+  it("does something", function() {
+    expect(orange).toBe('grape');
   });
 });
