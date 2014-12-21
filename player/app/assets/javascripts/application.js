c1808397b4e027e391602024880dc38a10220022
maggie <myustinreyes@gmail.com>
2014-12-21 14:13:47 -0500

    getting user oauth started

--- player/app/assets/javascripts/application.js
+++ player/app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require player
+//= require user_oath
 //= require_tree .
---
+++ player/app/assets/javascripts/user_oath.js
@@ -0,0 +1,5 @@
+SC.connect(function() {
+  SC.get('/me', function(me) { 
+    alert('Hello, ' + me.username); 
+  });
+});
