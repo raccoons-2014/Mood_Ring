1e218ca446c5f8e8495713ab0b4aebac821c653e
maggie <myustinreyes@gmail.com>
2014-12-22 10:37:15 -0500

    getting user routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   match "/welcome/play" => "welcome#play", :as => 'play', via: [:get, :post]
+
+  get '/connect' => 'users#new', as: :users
+  post '/connect' => 'users#create'
+  get '/disconnect' => 'users#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
