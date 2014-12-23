0dff0fe7e8ed40fc87c913c466d41a78138a048f
patriciacarbajal <carbajal.patricia@gmail.com>
2014-12-23 17:11:23 -0500

    moved app from player folder to root folder

---
+++ app/models/playlist.rb
@@ -0,0 +1,5 @@
+class Playlist < ActiveRecord::Base
+  def query_soundcloud(genre)
+    @tracks = CLIENT.get('/tracks', limit: 10, tags: genre)
+  end
+end
---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+CLIENT = SoundCloud.new({
+  :client_id     => ENV['SOUNDCLOUD_API'],
+  :client_secret => ENV['SOUNDCLOUD_SECRET']
+})
+
