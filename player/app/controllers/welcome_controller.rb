b3efd62eb22c7b195b5526e84719f7ef0de7e93f
patriciacarbajal <carbajal.patricia@gmail.com>
2014-12-23 17:18:26 -0500

    move app to root

--- player/app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    query_soundcloud(params[:landing_genre])
-    render :index
-  end
-end
--- player/app/helpers/welcome_helper.rb
+++
@@ -1,6 +0,0 @@
-module WelcomeHelper
-  def query_soundcloud(param)
-    @tracks = CLIENT.get('/tracks', limit: 10, tags: param)
-  end
-
-end
