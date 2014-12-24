79bfc35bd2618ba7d35f998cdbd02b52a4094e3d
patriciacarbajal <carbajal.patricia@gmail.com>
2014-12-24 16:40:59 -0500

    added view for genre buttons, but multi-filter search still not working correctly

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,4 +1,4 @@
-function PlayerWidget(sourceSelector, genre) {
+function PlayerWidget(sourceSelector) {
   this.sourceSelector = sourceSelector;
   this.genre = genre_choice;
   this.trackUrls = [];
@@ -13,13 +13,11 @@ PlayerWidget.prototype.getTagName= function() {
 
 //get song array
 PlayerWidget.prototype.populateTrackInfo= function(sourceSelector, genre) {
-  SC.get('/tracks', { q: sourceSelector.toLowerCase(), genres: genre, duration: {from: 180000 } }, function(tracks) {
+  SC.get('/tracks', { tags: sourceSelector.toLowerCase(), genre: genre, types: "original", duration: {from: 180000 } }, function(tracks) {
     for (i = 0; i < tracks.length; i++) {
       var random_track = Math.floor(Math.random() * (tracks.length - 1));
-      if (tracks[random_track].playback_count > 15000){
         this.trackTitles.push(tracks[random_track].title);
         this.trackUrls.push(tracks[random_track].stream_url);
-      };
     };
 
     this.current_track_title = this.trackTitles[0];
