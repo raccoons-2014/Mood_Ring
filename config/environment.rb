5250dd10e9855e9d4e22f1f796048784b342741f
patriciacarbajal <carbajal.patricia@gmail.com>
2014-12-26 18:36:39 -0500

    added echonest api

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ECHONEST = 
+
 CLIENT = SoundCloud.new({
   :client_id     => ENV['SOUNDCLOUD_API'],
   :client_secret => ENV['SOUNDCLOUD_SECRET']
