d95d137b9b91f7899b898a4f0ae8b7f1f0019d4b
maggie <myustinreyes@gmail.com>
2014-12-28 17:05:26 -0500

    prepping to merge logic into development

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,28 +14,21 @@ class UsersController < ApplicationController
           :soundcloud_access_token  => soundcloud_client.access_token,
           :soundcloud_refresh_token => soundcloud_client.refresh_token,
           :soundcloud_expires_at    => soundcloud_client.expires_at,
-        })
-      end
+      })
+    end
     redirect_to root_path
-    puts "****************************"
-      p @current_user
-      p logged_in?
   end
 
   def disconnect
     logout
     redirect_to root_path
-    puts "((((((((((((((((((((("
-      p @current_user
-      p logged_in?
   end
   
 private
   
-  #hardcoded parameters for the soundcloud_client. The redirect uri in our app is /soundcloud-connected
   def soundcloud_client
     return @soundcloud_client if @soundcloud_client
     @soundcloud_client = User.soundcloud_client(:redirect_uri  => soundcloud_connected_url)
   end
-
+  
 end
