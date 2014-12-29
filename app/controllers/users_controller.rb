c167d182e2a6f93c46164a46a219974eb27ee28e
maggie <myustinreyes@gmail.com>
2014-12-29 13:01:55 -0500

    got connect/disconnect buttons working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,23 @@ class UsersController < ApplicationController
 	end
 
   def connected
+
 	  if params[:error].nil?
-		  soundcloud_client.exchange_token(:code => params[:code])
+
+      soundcloud_client.exchange_token(:code => params[:code])
       user = soundcloud_client.get("/me")
 
-		  login(User.find_or_create_by(soundcloud_user_id: user.id))
+      login(User.find_or_create_by(soundcloud_user_id: user.id, soundcloud_username: user.username))
 
-    	@current_user.update_attributes!({
+    	user.update_attributes!({
           :soundcloud_access_token  => soundcloud_client.access_token,
           :soundcloud_refresh_token => soundcloud_client.refresh_token,
           :soundcloud_expires_at    => soundcloud_client.expires_at,
       })
+      puts "********************"
+      p user
     end
-    puts "*********************"
-    p @current_user
-    p logged_in?
+
     redirect_to root_path
   end
 
@@ -33,5 +35,4 @@ private
     return @soundcloud_client if @soundcloud_client
     @soundcloud_client = User.soundcloud_client(:redirect_uri  => soundcloud_connected_url)
   end
-  
 end
