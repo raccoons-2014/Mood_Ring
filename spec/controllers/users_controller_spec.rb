4c55ce1471f7578a1eae2551649fb947d9b3f425
maggie <myustinreyes@gmail.com>
2014-12-30 15:48:44 -0500

    created basic tests for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,7 +5,21 @@ describe UsersController, :type => :controller do
   context "after you click the connect button" do 
   	it "should redirect back to soundcloud" do 
       get :connect
-      expect(response).to have_http_status 302
+      expect(response).to have_http_status(:redirect)
   	end
+  end
+
+  context "once you connect with soundcloud" do 
+    xit "should redirect you back to the index page" do 
+      get :connected
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+
+  context "after you click the disconnect button" do 
+    it "should redirect you back to the welcome page" do 
+      get :disconnect 
+      expect(response).to have_http_status(:redirect)
+    end
   end
 end
