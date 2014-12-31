39de2833be5e3b0421a37ed903b1fd7766dd1682
Patricia Carbajal <carbajal.patricia@gmail.com>
2014-12-31 15:56:47 -0500

    three setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require underscore
+//= require three.min
 //= require turbolinks
 //= require main
 //= require player
 //= require echonest
+//= require three
 //= require_tree .
