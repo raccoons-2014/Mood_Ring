b27ec92e93e512cc044a1d269a9b75255a095fa0
Patricia Carbajal <carbajal.patricia@gmail.com>
2014-12-31 15:56:47 -0500

    three setup

---
+++ app/assets/javascripts/sphere.js
@@ -0,0 +1,19 @@
+// set up the sphere vars
+var radius = 50,
+    segments = 16,
+    rings = 16;
+
+// create a new mesh with
+// sphere geometry - we will cover
+// the sphereMaterial next!
+var sphere = new THREE.Mesh(
+
+  new THREE.SphereGeometry(
+    radius,
+    segments,
+    rings),
+
+  sphereMaterial);
+
+// add the sphere to the scene
+scene.add(sphere);
