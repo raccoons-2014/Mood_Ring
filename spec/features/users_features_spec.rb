892221143265b9dcad1616ebbbd33ee45f3f184e
maggie <myustinreyes@gmail.com>
2014-12-31 13:34:09 -0500

    got rid of unused tests

--- spec/features/users_features_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-feature 'Oauth Soundcloud Signin' do 
-  scenario 'User must sign into Soundcloud' do 
-  	visit soundcloud_callback_path
-
-  	expect(page).to have_text('Mood Ring')
-  end
-end
--- spec/features/welcome_features_spec.rb
+++ spec/features/welcome_features_spec.rb
@@ -13,12 +13,4 @@ feature 'Music Player' do
   	click_button 'punk'
   	expect(page).to have_text("What mood of music are you feeling?")
   end
-
-  scenario 'selecting a mood after selecting genre' do 
-  	visit root_path 
-
-  	click_button 'classical'
-  	click_button 'Happy'
-  	expect(page).to not_have_text("Choose or enter a mood")
-  end
 end
