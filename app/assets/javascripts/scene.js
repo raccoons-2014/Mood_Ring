f711fc44a39d7d7b9c60d30944d8a94f0eed4da5
Patricia Carbajal <carbajal.patricia@gmail.com>
2014-12-31 19:20:13 -0500

    add images

--- app/assets/javascripts/scene.js
+++ app/assets/javascripts/scene.js
@@ -1,115 +1,115 @@
-// set the scene size
-var WIDTH = window.innerWidth,
-  HEIGHT = window.innerHeight;
+// // set the scene size
+// var WIDTH = window.innerWidth,
+//   HEIGHT = window.innerHeight;
 
-// set some camera attributes
-var VIEW_ANGLE = 45,
-  ASPECT = WIDTH / HEIGHT,
-  NEAR = 0.1,
-  FAR = 10000;
+// // set some camera attributes
+// var VIEW_ANGLE = 45,
+//   ASPECT = WIDTH / HEIGHT,
+//   NEAR = 0.1,
+//   FAR = 10000;
 
-// get the DOM element to attach to
-// - assume we've got jQuery to hand
-var $container = $('#three');
+// // get the DOM element to attach to
+// // - assume we've got jQuery to hand
+// var $container = $('#three');
 
-// create a WebGL renderer, camera
-// and a scene
-var renderer = new THREE.WebGLRenderer();
-var camera =
-  new THREE.PerspectiveCamera(
-    VIEW_ANGLE,
-    ASPECT,
-    NEAR,
-    FAR);
+// // create a WebGL renderer, camera
+// // and a scene
+// var renderer = new THREE.WebGLRenderer();
+// var camera =
+//   new THREE.PerspectiveCamera(
+//     VIEW_ANGLE,
+//     ASPECT,
+//     NEAR,
+//     FAR);
 
-var scene = new THREE.Scene();
+// var scene = new THREE.Scene();
 
-// add the camera to the scene
-scene.add(camera);
+// // add the camera to the scene
+// scene.add(camera);
 
-// the camera starts at 0,0,0
-// so pull it back
-camera.position.z = 300;
+// // the camera starts at 0,0,0
+// // so pull it back
+// camera.position.z = 300;
 
-// start the renderer
-renderer.setSize(WIDTH, HEIGHT);
+// // start the renderer
+// renderer.setSize(WIDTH, HEIGHT);
 
-// attach the render-supplied DOM element
-$container.append(renderer.domElement);
+// // attach the render-supplied DOM element
+// $container.append(renderer.domElement);
 
-// set up the sphere vars
-var radius = 50,
-    segments = 16,
-    rings = 16;
+// // set up the sphere vars
+// var radius = 50,
+//     segments = 16,
+//     rings = 16;
 
-// create a new mesh with
-// sphere geometry - we will cover
-// the sphereMaterial next!
-var sphere = new THREE.Mesh(
+// // create a new mesh with
+// // sphere geometry - we will cover
+// // the sphereMaterial next!
+// var sphere = new THREE.Mesh(
 
-  new THREE.SphereGeometry(
-    radius,
-    segments,
-    rings),
+//   new THREE.SphereGeometry(
+//     radius,
+//     segments,
+//     rings),
 
-  sphereMaterial);
+//   sphereMaterial);
 
-// add the sphere to the scene
-scene.add(sphere);
-
-
-// create the sphere's material
-var sphereMaterial =
-  new THREE.MeshLambertMaterial(
-    {
-      color: 0xCC0000
-    });
-
- // create a point light
-var pointLight =
-  new THREE.PointLight(0xFFFFFF);
-
-// set its position
-pointLight.position.x = 10;
-pointLight.position.y = 50;
-pointLight.position.z = 130;
-
-// add to the scene
-scene.add(pointLight);
+// // add the sphere to the scene
+// scene.add(sphere);
 
 
-// draw!
-// renderer.render(scene, camera);
-var render = function () {
-        requestAnimationFrame( render );
+// // create the sphere's material
+// var sphereMaterial =
+//   new THREE.MeshLambertMaterial(
+//     {
+//       color: 0xCC0000
+//     });
 
-        sphere.rotation.x += 0.1;
-        sphere.rotation.y += 0.1;
+//  // create a point light
+// var pointLight =
+//   new THREE.PointLight(0xFFFFFF);
 
-        renderer.render(scene, camera);
-      };
+// // set its position
+// pointLight.position.x = 10;
+// pointLight.position.y = 50;
+// pointLight.position.z = 130;
 
-      render();
+// // add to the scene
+// scene.add(pointLight);
 
-// sphere geometry
-sphere.geometry
 
-// which contains the vertices and faces
-sphere.geometry.vertices // an array
-sphere.geometry.faces // also an array
+// // draw!
+// // renderer.render(scene, camera);
+// var render = function () {
+//         requestAnimationFrame( render );
 
-// its position
-sphere.position // contains x, y and z
-sphere.rotation // same
-sphere.scale // ... same
+//         sphere.rotation.x += 0.1;
+//         sphere.rotation.y += 0.1;
 
-// set the geometry to dynamic
-// so that it allow updates
-sphere.geometry.dynamic = true;
+//         renderer.render(scene, camera);
+//       };
 
-// changes to the vertices
-sphere.geometry.verticesNeedUpdate = true;
+//       render();
 
-// changes to the normals
-sphere.geometry.normalsNeedUpdate = true;
+// // sphere geometry
+// sphere.geometry
+
+// // which contains the vertices and faces
+// sphere.geometry.vertices // an array
+// sphere.geometry.faces // also an array
+
+// // its position
+// sphere.position // contains x, y and z
+// sphere.rotation // same
+// sphere.scale // ... same
+
+// // set the geometry to dynamic
+// // so that it allow updates
+// sphere.geometry.dynamic = true;
+
+// // changes to the vertices
+// sphere.geometry.verticesNeedUpdate = true;
+
+// // changes to the normals
+// sphere.geometry.normalsNeedUpdate = true;
 
