094c163e6e7fe050682645385e02af668c665229
maggie <myustinreyes@gmail.com>
2015-01-01 13:50:18 -0500

    fixed tests to pass again

--- spec/features/welcome_features_spec.rb
+++ spec/features/welcome_features_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 feature 'Music Player' do 
   scenario 'selecting a genre is possible' do 
-  	visit root_path
+  	visit player_path
 
   	expect(page).to have_text("genre")
   end
 
   scenario 'after selecting a genre, select mood' do 
-  	visit root_path
+  	visit player_path
 
   	click_button 'punk'
   	expect(page).to have_text("What mood of music are you feeling?")
