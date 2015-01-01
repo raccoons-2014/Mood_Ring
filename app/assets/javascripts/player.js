f3c5e616671a7a593b1677a77081dbea0541d606
patriciacarbajal <carbajal.patricia@gmail.com>
2015-01-01 14:18:52 -0500

    moved button to welcome view - wip

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -26,11 +26,7 @@ PlayerWidget.prototype.setCurrentTrack = function() {
   this.current_track_title = this.trackTitles.shift();
   this.current_track_url = this.trackUrls.shift();
   this.current_track_id = this.trackIds.shift();
-<<<<<<< HEAD
   $("#playlist").html(" <h1> Now playing: <br> " + this.current_track_title + " </h1>");
-=======
-  $("#playlist").html(" <h1> Now playing: <br> " + this.current_track_title + " </h1> <button id='fav'>Favorite</button>");
->>>>>>> adding favorite button - wip
 };
 
 PlayerWidget.prototype.resetCurrentTrack = function() {
@@ -70,9 +66,13 @@ PlayerWidget.prototype.nextSongFetch = function() {
 
 PlayerWidget.prototype.favoriteTrack = function() {
 <<<<<<< HEAD
+<<<<<<< HEAD
   console.log("in favoriteTrack")
 =======
 >>>>>>> adding favorite button - wip
+=======
+  console.log("in favoriteTrack")
+>>>>>>> moved button to welcome view - wip
   SC.connect(function() {
     // favorite the track with current track id
     console.log(this.current_track_id)
