adb1f69dec64a9f70da0633752b7b32bdae7846c
maggie <myustinreyes@gmail.com>
2015-01-02 16:41:45 -0500

    modifying databases

--- db/migrate/20150102214031_create_genres.rb
+++ db/migrate/20150102214031_create_genres.rb
@@ -1,7 +1,7 @@
 class CreateGenres < ActiveRecord::Migration
   def change
     create_table :genres do |t|
-      t.string     :name, default: "Unknown"
+      t.string   :type, default: "Unknown"
 
       t.timestamps
     end
