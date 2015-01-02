8a646b4b20ab615fc6043a5d02131480c04bd0ca
maggie <myustinreyes@gmail.com>
2015-01-02 17:22:16 -0500

    sticking with one database

--- db/migrate/20150102193258_create_songs.rb
+++ db/migrate/20150102193258_create_songs.rb
@@ -1,12 +1,12 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      t.string	   :title, null: false
-      t.string     :artist, null: false
-      t.string     :stream_url, null: false
-      t.string     :album_art, null: false
-      t.references :genre
-      t.references :mood
+      t.string	 :title, null: false
+      t.string   :artist, null: false
+      t.string   :stream_url, null: false
+      t.string   :album_art, null: false
+      t.string   :genre, default: "Unknown"
+      t.string   :mood, null: false
 
       t.timestamps
     end
--- db/migrate/20150102195659_create_moods.rb
+++
@@ -1,9 +0,0 @@
-class CreateMoods < ActiveRecord::Migration
-  def change
-    create_table :moods do |t|
-      t.string     :feeling, null: false
-
-      t.timestamps
-    end
-  end
-end
