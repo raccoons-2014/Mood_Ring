f541af7472bbb640a5f95a2a35d7880a40a0d0c7
maggie <myustinreyes@gmail.com>
2015-01-02 13:24:50 -0500

    rewrote over old tests

--- spec/features/welcome_features_spec.rb
+++ spec/features/welcome_features_spec.rb
@@ -1,16 +1,9 @@
 require 'rails_helper'
 
 feature 'Music Player' do 
-  scenario 'selecting a genre is possible' do 
+  scenario 'you must be signed in to use the music player' do 
   	visit player_path
 
-  	expect(page).to have_text("genre")
-  end
-
-  scenario 'after selecting a genre, select mood' do 
-  	visit player_path
-
-  	click_button 'punk'
-  	expect(page).to have_text("What mood of music are you feeling?")
+  	expect(page).to_not have_content("genre")
   end
 end
