34827d657cd3ac9a9ae0c432fd56793d114945bc
maggie <myustinreyes@gmail.com>
2015-01-02 17:28:25 -0500

    modified databased

--- db/schema.rb
+++ db/schema.rb
@@ -11,30 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102214031) do
+ActiveRecord::Schema.define(version: 20150102193258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "genres", force: true do |t|
-    t.string   "kind",       default: "Unknown"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "moods", force: true do |t|
-    t.string   "feeling",    null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "songs", force: true do |t|
-    t.string   "title",      null: false
-    t.string   "artist",     null: false
-    t.string   "stream_url", null: false
-    t.string   "album_art",  null: false
-    t.integer  "genre_id"
-    t.integer  "mood_id"
+    t.string   "title",                          null: false
+    t.string   "artist",                         null: false
+    t.string   "stream_url",                     null: false
+    t.string   "album_art",                      null: false
+    t.string   "genre",      default: "Unknown"
+    t.string   "mood",                           null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
