b59f3ae98fd6a99ec76484c61d252a99dc854601
Karina Carmona <karinacarmona@me.com>
2015-01-04 16:50:25 -0500

    pseudo =code for audiocontroller finished

--- app/assets/javascripts/audio.js
+++ app/assets/javascripts/audio.js
@@ -1,6 +1,35 @@
+//perhaps need to delete other instance of AudioCOntroller when
+//switching moods in main.js
+
 function AudioController(audioSelector) {
   //find the selector for the audio player
   //maybe selector play buttons, progressbar, etc.
 }
 
-AudioController.prototype.updateSource
+AudioController.prototype.grabPlaylist = function() {
+//do AvticeRecord query gram stream URLs from db and
+//populate track info
+// also populate track info aka title and maybe url to access favorites?
+};
+
+AudioController.prototype.setCurrentTrack = function() {
+//update Track info in margquee
+//progress to current song or next song in array of stream urls
+};
+
+AudioController.prototype.updateVisualizerSource = function() {
+// call Vizualizer and udate song_url
+};
+
+AudioController.prototype.playSong = function () {
+//should call update Visualizer Source
+//and control player play button
+};
+
+AudioController.prototype.pauseSong = function() {
+//pause song. and pause Visualizer.
+};
+
+AudioController.prototype.progressBar = function() {
+//self explanatory
+}
