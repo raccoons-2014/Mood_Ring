6c645936395e6761a8a3c0be6b86be5fb7b26b4f
Patricia Carbajal <carbajal.patricia@gmail.com>
2015-01-04 16:30:20 -0500

    redirect test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,9 @@ class UsersController < ApplicationController
           :soundcloud_expires_at    => soundcloud_client.expires_at,
       })
     end
+    p "**************"
+    p user
+    p "**************"
     session[:user_id] = user.id
     redirect_to root_path
   end
