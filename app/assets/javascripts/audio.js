66e3dc28b771cfefdec2963bfd82b5823829c51a
Karina Carmona <karinacarmona@me.com>
2015-01-05 11:06:49 -0500

    clean up commented out code and deleted useless functions

--- app/assets/javascripts/audio.js
+++ app/assets/javascripts/audio.js
@@ -1,9 +1,6 @@
-//perhaps need to delete other instance of AudioCOntroller when
-//switching moods in main.js
+//to control player and visualizer at the same time
 
 function AudioController() {
-  //find the selector for the audio player
-  //maybe selector play buttons, progressbar, etc.
   this.trackPlaylist = ['https://api.soundcloud.com/tracks/146159376/stream?client_id=c751293c35f7cb00b48ee6383ea84aa6','https://api.soundcloud.com/tracks/96379023/stream?client_id=c751293c35f7cb00b48ee6383ea84aa6', 'https://api.soundcloud.com/tracks/120682891/stream?client_id=c751293c35f7cb00b48ee6383ea84aa6'];
   this.trackTitles = [];
   this.trackCount = this.trackPlaylist.length;
@@ -28,8 +25,6 @@ AudioController.prototype.grabPlaylist = function() {
 // also populate track info aka title and maybe url to access favorites?
 };
 
-//update Track info in margquee
-//progress to current song or next song in array of stream urls
 AudioController.prototype.setNextTrack = function() {
   if ((this.trackNumber+1) < this.trackCount) {
     this.trackNumber++;
@@ -51,31 +46,16 @@ AudioController.prototype.setUpSource = function (song_url) {
 
 }
 
-AudioController.prototype.changePlayerSource = function() {
-  var source = document.getElementById("mp3source");
-  source.src = this.currentTrackUrl;
-};
-
-// call Vizualizer and udate song_url
-AudioController.prototype.updateVisualizerSource = function() {
-
-};
-
 AudioController.prototype.getFrequencyData = function() {
   this.analyser.getByteFrequencyData(this.dataArray);
-
-      for(var i = 0; i < this.bufferLength; i++) {
-        averageFrequency += this.dataArray[i];
-      };
-
+    for(var i = 0; i < this.bufferLength; i++) {
+      averageFrequency += this.dataArray[i];
+    };
   //find average
   averageFrequency = averageFrequency / this.bufferLength;
   return averageFrequency;
 }
 
-
-//should call update Visualizer Source
-//and control player play button
 AudioController.prototype.playerControls = function () {
 $("#play").click(function(){
    source.mediaElement.play();
@@ -96,8 +76,6 @@ $("#next").click(function(){
 }.bind(this));
 
 };
-
-
 
 AudioController.prototype.progressBar = function() {
 //self explanatory
