0dd8879e6fedd825a9129d4fc89aaffb437c9bd0
Karina Carmona <karinacarmona@me.com>
2015-01-05 10:48:09 -0500

    delete unneeded variables

--- app/assets/javascripts/audio.js
+++ app/assets/javascripts/audio.js
@@ -6,8 +6,6 @@ function AudioController() {
   //maybe selector play buttons, progressbar, etc.
   this.trackPlaylist = ['https://api.soundcloud.com/tracks/146159376/stream?client_id=c751293c35f7cb00b48ee6383ea84aa6','https://api.soundcloud.com/tracks/96379023/stream?client_id=c751293c35f7cb00b48ee6383ea84aa6', 'https://api.soundcloud.com/tracks/120682891/stream?client_id=c751293c35f7cb00b48ee6383ea84aa6'];
   this.trackTitles = [];
-  this.currentTrackUrl = 'https://api.soundcloud.com/tracks/184210017/stream?client_id=c751293c35f7cb00b48ee6383ea84aa6';
-  this.currentTrackTitle = "";
   this.trackCount = this.trackPlaylist.length;
   this.trackNumber = 0;
 
@@ -30,19 +28,18 @@ AudioController.prototype.grabPlaylist = function() {
 // also populate track info aka title and maybe url to access favorites?
 };
 
-AudioController.prototype.setNextTrack = function() {
 //update Track info in margquee
 //progress to current song or next song in array of stream urls
-
-if ((this.trackNumber+1) < this.trackCount) {
-  this.trackNumber++;
-  song.src = this.trackPlaylist[this.trackNumber];
-  source.mediaElement.play();
-} else{
-  this.trackNumber = 0;
-  song.src = this.trackPlaylist[this.trackNumber];
-  source.mediaElement.play();
-}
+AudioController.prototype.setNextTrack = function() {
+  if ((this.trackNumber+1) < this.trackCount) {
+    this.trackNumber++;
+    song.src = this.trackPlaylist[this.trackNumber];
+    source.mediaElement.play();
+  } else{
+    this.trackNumber = 0;
+    song.src = this.trackPlaylist[this.trackNumber];
+    source.mediaElement.play();
+  }
 };
 
 AudioController.prototype.setUpSource = function (song_url) {
