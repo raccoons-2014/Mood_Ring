754f937b3f7a5f0f0581aa392701484ed78399bf
maggie <myustinreyes@gmail.com>
2015-01-04 19:29:38 -0500

    adding favorite button to page

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -65,20 +65,14 @@ PlayerWidget.prototype.nextSongFetch = function() {
 }
 
 PlayerWidget.prototype.favoriteTrack = function() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  console.log("in favoriteTrack")
-=======
->>>>>>> adding favorite button - wip
-=======
-  console.log("in favoriteTrack")
->>>>>>> moved button to welcome view - wip
+  $('#like').on('click', function(){
   SC.connect(function() {
     // favorite the track with current track id
     console.log(this.current_track_id)
     SC.put('/me/favorites/' + this.current_track_id );
     console.log("favorite success")
     });
+  })
 }
 
 
