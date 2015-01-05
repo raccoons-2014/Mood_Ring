84023ca9e5eae16ed0607fb4505eb8c6bdbef9ef
dakotameadows <dakota.w.meadows@gmail.com>
2015-01-04 20:43:56 -0500

    got rid of user id

--- db/migrate/20141226213855_create_users.rb
+++ db/migrate/20141226213855_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string    :soundcloud_access_token
       t.string    :soundcloud_refresh_token
       t.datetime  :soundcloud_expires_at
-      
+
       t.timestamps
     end
   end
