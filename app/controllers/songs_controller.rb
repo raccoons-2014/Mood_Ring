eee9eb22a11d6f7fdd13aa6be8f2658d97a23c53
Karina Carmona <karinacarmona@me.com>
2015-01-05 17:52:27 -0500

    implemented main.js and songs_controller changes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,28 +2,16 @@ class SongsController < ApplicationController
   respond_to :html, :js
 
   def new
+    @post = Post.new
     respond_to do |format|
-      format.html {
-        @post = Post.new
-      }
-      format.js {
-        @post = Post.new
-      }
+      format.html
+      format.js
     end
   end
 
   def create
-    @song = Song.new(title: params[:title], stream_url: params[:stream_url], mood: params[:mood])
-    respond_to do |format|
-      format.html {
-        @song.save
-        redirect_to root_path
-      }
-      format.js {
-        @song.save
-        redirect_to root_path
-      }
-    end
+    Song.create(title: params[:title], stream_url: params[:stream_url], mood: params[:mood])
+    redirect_to root_path
   end
 
   def index
