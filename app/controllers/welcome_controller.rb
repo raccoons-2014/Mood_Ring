e2e07c6ab885730572b9700ba028b207e574aec6
Karina Carmona <karinacarmona@me.com>
2015-01-05 11:13:59 -0500

    deleted connect route and commented out code in player route

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,7 @@
 class WelcomeController < ApplicationController
 
-  def connect
-  	render :connect
-  end
-
   def player
-  	# if current_user
-      # render :player
-  	# else
-  	#   redirect_to root_path
-  	# end
+    render :player
   end
 
 end
