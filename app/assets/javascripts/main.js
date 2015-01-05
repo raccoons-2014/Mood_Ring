3285fc504873e588d955d3ace6bad5a2ebffa74c
dakotameadows <dakota.w.meadows@gmail.com>
2015-01-04 21:36:46 -0500

    cleaned up main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -72,9 +72,6 @@ $(document).ready(function(){
     var stream_url = $("#songList input[name='song']:checked")[0].value;
     var title = $("#songList input[name='song']:checked").parent().text();
     var mood = $('#moodDropdown option:selected').text();
-    console.log(stream_url);
-    console.log(title);
-    console.log(mood);
 
     $.ajax ({
       url: 'songs/create',
@@ -85,40 +82,6 @@ $(document).ready(function(){
       $('#songList').empty();
       $('#moodDropdown').empty();
       $('#submit').empty();
-      console.log("done");
-    })
-  });
-
-    $('#ajax').empty();
-    // $('#songList').empty();
-    // $('#moodDropdown').empty();
-    SC.get('/tracks', {q: $('#text').val()}, function(tracks) {
-      for (i = 0; i < tracks.length; i++) {
-        $('#songList').append("<label><input type='radio' name='song' value =" + tracks[i].stream_url + ">" + tracks[i].title + "</label><br>");
-      }
-    });
-    $('#moodDropdown').append("<select id = 'dropDownList'><option value='sad'>Sad</option><option value='happy'>Happy</option><option value='angry'>Angry</option><option value='F DA POLICE'>F DA POLICE</option></select>");
-
-
-  $('#ajax').on("click", function(event){
-    event.preventDefault();
-    var stream_url = $("#songList input[name='song']:checked")[0].value;
-    var title = $("#songList input[name='song']:checked").parent().text();
-    var mood = $('#moodDropdown option:selected').text();
-    console.log(stream_url);
-    console.log(title);
-    console.log(mood);
-
-    $.ajax ({
-      url: 'songs/create',
-      data: {title: title, stream_url: stream_url, mood: mood},
-      dataType: "json",
-      type: "POST"
-    }).done(function() {
-      $('#songList').empty();
-      $('#moodDropdown').empty();
-      $('#submit').empty();
-      console.log("done");
     })
   });
 
