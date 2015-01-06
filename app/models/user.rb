97240bec91492d5d51b706824b08636e3ce24001
maggie <myustinreyes@gmail.com>
2015-01-06 12:04:41 -0500

    got rid of logic in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,3 @@
 class User < ActiveRecord::Base
   validates :soundcloud_user_id, presence: true, uniqueness: true
-
-  def self.soundcloud_client(options={})
-    options = {
-      :client_id     => CLIENT.client_id,
-      :client_secret => CLIENT.client_secret,
-    }.merge(options)
-
-    Soundcloud.new(options)
-  end
-
-
-  def soundcloud_client(options={})
-    options= {
-      :expires_at    => soundcloud_expires_at,
-      :access_token  => soundcloud_access_token,
-      :refresh_token => soundcloud_refresh_token
-    }.merge(options)
-
-    client = self.class.soundcloud_client(options)
-
-    client.on_exchange_token do
-      self.update_attributes!({
-        :soundcloud_access_token  => client.access_token,
-        :soundcloud_refresh_token => client.refresh_token,
-        :soundcloud_expires_at    => client.expires_at,
-      })
-    end
-
-    client
-  end
 end
