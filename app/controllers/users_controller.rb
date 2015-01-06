1f62265bc544e4b37987501d1a7543c4f76a8850
maggie <myustinreyes@gmail.com>
2015-01-06 11:10:28 -0500

    got rid of print statements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,6 @@ class UsersController < ApplicationController
           :soundcloud_expires_at    => soundcloud_client.expires_at,
       })
     end
-    p "**************"
-    p user
-    p "**************"
     session[:user_id] = user.id
     redirect_to root_path
   end
