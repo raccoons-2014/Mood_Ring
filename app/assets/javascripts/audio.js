5a3b698cca59d40c945bbc869f248217a543e280
Karina Carmona <karinacarmona@me.com>
2015-01-07 16:24:49 -0500

    changed the breing up search button position

--- app/assets/javascripts/audio.js
+++ app/assets/javascripts/audio.js
@@ -83,12 +83,13 @@ AudioController.prototype.glowingRing = function() {
   $('.glowing-ring').click(function(e) {
     e.preventDefault();
     Slides.show('chooseMood');
-    bringUpSearchButton();
+
     var clickedMood = $(this).data("mood");
     MoodDb.getSong(clickedMood)
     .then(function(response){
       response = _.shuffle(response);
       glowplayer.getNewTracks(response);
+      bringUpSearchButton();
     });
   });
 }
