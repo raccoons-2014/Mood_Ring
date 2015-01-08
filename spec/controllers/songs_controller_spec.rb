71934c42f7ac1197bdf95dd398e38aa21b0a7598
Patricia Carbajal <carbajal.patricia@gmail.com>
2015-01-07 20:22:24 -0500

    wrote tests for song controller and removed test folder

---
+++ spec/controllers/songs_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe SongsController, :type => :controller do
+  let(:song1) { Song.create(title: "Service And Sacrifice", stream_url: "https://api.soundcloud.com/tracks/164353339/stream", mood: 'Melancholy')}
+  let(:song2) {Song.create(title: "NeYo - Mad", stream_url: "https://api.soundcloud.com/tracks/85986175/stream", mood: 'Melancholy')}
+  let(:song3) {Song.create(title: "how to never stop being sad", stream_url: "https://api.soundcloud.com/tracks/71204300/stream", mood: 'Melancholy')}
+  describe "GET index" do
+    it "retrieves songs with the selected mood" do
+      songs = Song.where(mood: 'Melancholy')
+      expect(songs.all{ |s| s.mood == 'Melancholy'}).to be_truthy
+    end
+  end
+
+describe "GET /listings.json" do
+  before do
+    get :index, format: :js
+  end
+
+  context 'all songs' do
+    it 'returns the songs' do
+      expect(json.collect{|l| ["title"]}).to include(songs[0].title)
+    end
+    end
+  end
+
+end
