1d74e7527302ff7bcfc8a901b78b7bf81f907887
maggie <myustinreyes@gmail.com>
2015-01-07 19:08:06 -0500

    working on tests

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe WelcomeController, :type => :controller do
-  context "when you go to the index page" do
-    it "should render the index template" do
-      get :index
+  context "when you go to the homepage" do
+    it "should render the homepage" do
+      get :homepage
       expect(response).to be_success
     end
   end
