6fb2726475bd4efa4e3ebdb811c41425d7b3db4b
Patricia Carbajal <carbajal.patricia@gmail.com>
2015-01-07 20:22:24 -0500

    wrote tests for song controller and removed test folder

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,23 +1,15 @@
 class SongsController < ApplicationController
   respond_to :html, :js
 
-  def new
-    @post = Post.new
-    respond_to do |format|
-      format.html
-      format.js
-    end
-  end
-
   def create
     Song.create(title: params[:title], stream_url: params[:stream_url], mood: params[:mood])
     redirect_to root_path
   end
 
   def index
-    songs = Song.where(mood: params[:mood])
+    @songs = Song.where(mood: params[:mood])
     respond_to do |format|
-      format.js {render :json => songs}
+      format.js {render :json => @songs}
     end
   end
 
